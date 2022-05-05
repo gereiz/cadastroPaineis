c8225b379e4da920f210115676691cde47ee7a4a
Georgie Reis <georgie.reis@farmaciaindiana.far.br>
2022-05-04 22:27:47 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
+//Route::get('/home', [App\Http\Controllers\OutdoorController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\OutdoorController::class, 'index'])->name('home');
 
 Route::get('/addOutdoorForm', [App\Http\Controllers\OutdoorController::class, 'addForm'])->name('add_form_outdoor');
